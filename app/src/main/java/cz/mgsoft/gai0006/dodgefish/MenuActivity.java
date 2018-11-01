e527f46aa0ba39ba25810b393737db5bf40de5e4
MGSE97 <elektrikar97@gmail.com>
2018-11-01 10:42:18 +0100

    Project created

---
+++ app/src/main/java/cz/mgsoft/gai0006/dodgefish/MenuActivity.java
@@ -0,0 +1,13 @@
+package cz.mgsoft.gai0006.dodgefish;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
